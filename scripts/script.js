73302450c660a208159c566c176d60f8a991bcd3
Leerlandais <leerlandais@gmail.com>
2023-11-22 20:42:33 +0100

    fixed it anyway

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var buttonQ1 = document.getElementById("q1");
     buttonQ1.addEventListener('click', function() {
         document.getElementById("intanswerJD").innerHTML = "New text!";
-        document.getElementById("intanswerMC").innerHTML = "New text!";
+   //     document.getElementById("intanswerMC").innerHTML = "New text!";
         console.log("Button clicked");
     })
 
