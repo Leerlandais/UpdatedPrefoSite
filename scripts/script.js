1d97e082f21d7501978e94855e7c9f3f629d253a
Leerlandais <leerlandais@gmail.com>
2023-11-22 20:34:28 +0100

    night night

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() { 
+
+
+
+    var buttonQ1 = document.getElementById("q1");
+    buttonQ1.addEventListener('click', function() {
+        document.getElementById("intanswerJD").innerHTML = "New text!";
+        document.getElementById("intanswerMC").innerHTML = "New text!";
+        console.log("Button clicked");
+    })
+
+
+});
